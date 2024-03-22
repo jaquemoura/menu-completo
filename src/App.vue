23c4db0c8f65e3463fb9350013e2ede598f504fe
jaquemoura <jaquelinemm1@hotmail.com>
2024-03-22 16:58:53 -0300

    menu mobile e desktop com três níveis

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MainMenu :n1menu="menu"  />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/menu/MainMenu.vue'
+import menujson from './api/data.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainMenu
+  },
+  data() {
+    return {
+      screenWidthDesktop: window.innerWidth > 1200,
+      screenWidthMob: window.innerWidth < 1200
+    }
+  },
+  computed: {
+    menu(){
+      return menujson.menu;
+    },
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  background: #fff;
+  margin: 0;
 }
 </style>
